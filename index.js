87c0c5534dd0a6438bc0997401b77d430e744610
supriyo nag <supriyonag522@gmail.com>
2025-05-17 17:24:40 +0530

    firs

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const app = express();
 const port = 3000;
 
 (async () => {
-  const empConn = await mongoose.createConnection('mongodb://localhost:27017/employees_db');
-  const prodConn = await mongoose.createConnection('mongodb://localhost:27017/products_db');
+  const empConn = await mongoose.createConnection('mongodb+srv://supriyonag552:z4ZqfvOc3OL01RnO@cluster0.nqqno4g.mongodb.net/employees_db?retryWrites=true&w=majority&appName=Cluster0/employees_db');
+  const prodConn = await mongoose.createConnection('mongodb+srv://supriyonag552:z4ZqfvOc3OL01RnO@cluster0.nqqno4g.mongodb.net/products_db?retryWrites=true&w=majority&appName=Cluster0/products_db');
 
   const combinedRoute = require('./routes/combined')(empConn, prodConn);
   app.use('/api', combinedRoute);
