81ba063120905abbfea103ecb0738ca4f5d2618b
supriyo nag <supriyonag522@gmail.com>
2025-05-17 17:18:56 +0530

    first

--- config/employeeDB.js
+++ config/employeeDB.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectEmployeeDB = async () => {
-  await mongoose.createConnection('mongodb://localhost:27017/employees_db', {
+  await mongoose.createConnection('mongodb+srv://supriyonag552:z4ZqfvOc3OL01RnO@cluster0.nqqno4g.mongodb.net/employees_db?retryWrites=true&w=majority&appName=Cluster0', {
     useNewUrlParser: true,
     useUnifiedTopology: true
   });
--- config/productDB.js
+++ config/productDB.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectProductDB = async () => {
-  await mongoose.createConnection('mongodb://localhost:27017/products_db', {
+  await mongoose.createConnection('mongodb+srv://supriyonag552:z4ZqfvOc3OL01RnO@cluster0.nqqno4g.mongodb.net/products_db?retryWrites=true&w=majority&appName=Cluster0', {
     useNewUrlParser: true,
     useUnifiedTopology: true
   });
